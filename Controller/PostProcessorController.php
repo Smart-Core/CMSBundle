12b700a96e123a482fe00ea8c09e28d6ea8b1b25
Smart-Core <artem.digi@gmail.com>
2012-05-21 15:25:43 +0700

    resetting

--- Controller/PostProcessorController.php
+++ Controller/PostProcessorController.php
@@ -11,12 +11,14 @@ class PostProcessorController extends Controller
 	public function indexAction($slug)
 	{
 		$this->init();
-		
+				
 		// @todo УБРАТЬ, это сейчас тут тесты с регистрацией...
 		if (isset($_POST['fos_user_registration_form']) or
 			isset($_POST['fos_user_profile_form']) or
 			isset($_POST['fos_user_resetting_form']) or
-			isset($_POST['fos_user_change_password_form'])
+			isset($_POST['fos_user_change_password_form']) or 
+			$this->container->get('request')->getBaseUrl() . '/' . $slug === $this->container->get('router')->generate('fos_user_resetting_send_email') or
+			$this->container->get('request')->getBaseUrl() . '/' . $slug === $this->container->get('router')->generate('fos_user_resetting_check_email')
 		) {
 			return $this->forward('SmartCoreEngineBundle:NodeMapper:index', array('slug' => $slug));
 		}
