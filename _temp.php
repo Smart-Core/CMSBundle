34fc92835575b8c08a90e68f6af7e6a24e018fee
Denis Averkov <chstudio@mail.ru>
2012-08-27 15:06:22 +0400

    test

--- _temp.php
+++ _temp.php
@@ -1,5 +1,5 @@
 <?php
-
+//for push
 function sc_is_absolute_path($path)
 {
     return (strpos($path, '/') === 0 or strpos($path, ':') === 1) ? true : false;
@@ -8,20 +8,20 @@ function sc_is_absolute_path($path)
 function sc_profile($Logger = null, $precision = 3)
 {
     $exec_time = microtime(true) - SMARTCORE_START_TIME;
-    
+
     echo '<hr />Execution time: <b>', round($exec_time, $precision) * 1000 , '</b> ms',
         '. Memory usage <b>' , round((memory_get_usage() - SMARTCORE_START_MEMORY_USAGE) / 1024 / 1024, 2), '</b> MB (<b>',
         round((memory_get_peak_usage(true) - SMARTCORE_START_MEMORY_USAGE) / 1024 / 1024, 2), '</b> peak).',
         ' Included files: <b>' . count(get_included_files()) . "</b>.\n";
-    
+
     if (!is_null($Logger) and is_object($Logger)) {
         echo '<br />DB query count: <b>' . $Logger->currentQuery . '</b>';
-        
+
         $queries_time = 0;
         foreach ($Logger->queries as $value) {
             $queries_time += $value['executionMS'];
         }
-        
+
         $delta = round($queries_time * 100 / $exec_time, 2);
         echo ' (summary execution time: <b>' . round($queries_time, $precision) * 1000 . "</b> ms, $delta %)." . "\n";
     }
