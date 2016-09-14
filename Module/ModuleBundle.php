ff8de3c560a7dda01af0c1c47986bea76d576fea
Smart-Core <artem.digi@gmail.com>
2016-09-15 02:27:46 +0700

    remove comments

--- Module/ModuleBundle.php
+++ Module/ModuleBundle.php
@@ -2,14 +2,8 @@
 
 namespace SmartCore\Bundle\CMSBundle\Module;
 
-//use Knp\RadBundle\AppBundle\Bundle;
-//use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Smart\CoreBundle\AppBundle\Bundle;
 
-/**
- * Использование Knp\RadBundle для автоматического конфигурирования сервисов,
- * если у модуля существует файл config/services.yml.
- */
 abstract class ModuleBundle extends Bundle
 {
     use ModuleBundleTrait;
