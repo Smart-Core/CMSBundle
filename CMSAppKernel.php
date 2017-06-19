3cc613f0fecdea6a43c6811c0aa8fcf4a4e6ee7b
Smart-Core <artem.digi@gmail.com>
2017-06-19 18:02:24 +0700

    upd

--- CMSAppKernel.php
+++ CMSAppKernel.php
@@ -190,6 +190,7 @@ abstract class CMSAppKernel extends BaseKernel
         //$bundles[] = new \Knp\Bundle\DisqusBundle\KnpDisqusBundle();
         $bundles[] = new \Knp\Bundle\MenuBundle\KnpMenuBundle();
         //$bundles[] = new \Knp\RadBundle\KnpRadBundle();
+        $bundles[] = new \Lsw\ApiCallerBundle\LswApiCallerBundle();
         $bundles[] = new \Liip\ThemeBundle\LiipThemeBundle();
         $bundles[] = new \Liip\ImagineBundle\LiipImagineBundle();
         $bundles[] = new \Misd\PhoneNumberBundle\MisdPhoneNumberBundle();
